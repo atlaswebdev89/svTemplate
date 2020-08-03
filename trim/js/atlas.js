4067e23369a67bec410e03da85a1b55c4427da03
Atlas <doroshuk33@yandex.by>
2020-08-03 14:51:39 +0300

    menu vertical

--- trim/js/atlas.js
+++ trim/js/atlas.js
@@ -15,16 +15,33 @@ $('.navbar-toggler').click(function () {
 	$('.global-overlay').addClass('overlay-open');
 	$('.offcanvas-navigation').addClass('menu-open');
 	$('body').addClass('body-open');
+	$('.navbar-toggler div').toggleClass("open");
+
 });
+
 $('.global-overlay').click(function () {
 	$('.global-overlay').removeClass('overlay-open');
 	$('.offcanvas-navigation').removeClass('menu-open');
 	$('body').removeClass('body-open');
+	$('.navbar-toggler div').toggleClass("open");
 });
 
 $('.btn-close').on('click', function(e){
 	e.preventDefault();
 	var $this = $(this);
 	 		$this.parents('.menu-open').removeClass('menu-open');
-		$($overlay).removeClass('overlay-open');
+			$($overlay).removeClass('overlay-open');
+			$('.navbar-toggler div').toggleClass("open");
+			$('body').toggleClass('body-open');
 	});
+
+
+//Сделать аккордеон одна только открыта и найти родительское под меню
+//Скрипт выделения активного меню
+$('span.menu-expand i').on('click', function (e) {
+	e.preventDefault();	
+		$('ul.sub-menu').slideToggle(250);
+});
+
+	
+

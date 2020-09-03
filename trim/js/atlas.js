5c8a7d49f4dc624669a927ca54a810d2da6ffe6f
atlas <doroshuk33@yandex.by>
2020-09-03 16:27:59 +0300

    update

--- trim/js/atlas.js
+++ trim/js/atlas.js
@@ -111,6 +111,30 @@ $('.navbar a, .offcanvas-menu a').each(function() {
 			return false;
 		}); 
 
-
+$(document).ready(function() {
+ 
+        $('.works').isotope({
+            itemSelector: '.work'
+        });
+ 
+        $('#filter a').click(function(){
+ 
+            $('#filter a').removeClass('current');
+            $(this).addClass('current');
+            var selector = $(this).attr('data-filter');
+ 
+            $('.works').isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 1000,
+                    easing: 'easeOutQuart',
+                    queue: false
+                }
+            });
+            return false;
+ 
+        });
+ 
+});
 
 
